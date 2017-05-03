a25064aa87345f5a0a5d9761f02b33e1b8a0a219
Jeremy Ary <jary@redhat.com>
2017-05-02 22:22:06 -0500

    adjusting routes to not inOut

--- Billing/src/main/java/com/redhat/refarch/microservices/billing/routes/BillingRoute.java
+++ Billing/src/main/java/com/redhat/refarch/microservices/billing/routes/BillingRoute.java
@@ -45,9 +45,9 @@ public class BillingRoute extends SpringRouteBuilder {
 
         from("amq:billing.orders.new")
                 .routeId("processNewOrders")
-//                .unmarshal(dataFormatFactory.formatter(Transaction.class))
+                .unmarshal(dataFormatFactory.formatter(Transaction.class))
                 .bean(billingService, "process");
-//                .marshal(dataFormatFactory.formatter(Result.class))
+//                .marshal(dataFormatFactory.formatter(Result.class));
 //                .convertBodyTo(String.class);
 
         from("amq:billing.orders.refund")
