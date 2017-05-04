ece9a779238b3579c73718683482d807739158ca
Jeremy Ary <jary@redhat.com>
2017-05-04 11:46:57 -0500

    adding in warehouse source context

--- Gateway/src/main/java/com/redhat/refarch/microservices/gateway/routes/GatewayRoute.java
+++ Gateway/src/main/java/com/redhat/refarch/microservices/gateway/routes/GatewayRoute.java
@@ -100,9 +100,13 @@ public class GatewayRoute extends SpringRouteBuilder {
         // calls to warehouse are used as Event Messages (InOnly) via active-mq for fault tolerance
         from("direct:warehouse")
                 .routeId("warehouseMsgGateway")
+                .log(LoggingLevel.INFO, "[${exchangeId}] entering warehouse: ")
+                .to("log:INFO?showBody=true&showHeaders=true")
 
                 // filter out transactions that failed or faulted out so we don't fulfill
                 .filter(simple("${body} contains 'SUCCESS'"))
+                    .log(LoggingLevel.INFO, "[${exchangeId}] FILTERED SUCCESS, FOWARDING TO WAREHOUSES")
+                    .to("log:INFO?showBody=true&showHeaders=true")
                     .inOnly("amq:topic:warehouse.orders&jmsMessageType=Text");
     }
 }
